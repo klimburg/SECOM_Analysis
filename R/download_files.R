6a59c4ef00a1ea8bc918248716d3fb441b9eddf3
Kevin Limburg <limburg.kevin@gmail.com>
2015-01-18 14:20:18 -0600

    modified download script for windows/*nix system compatibility

--- R/download_files.R
+++ R/download_files.R
@@ -1,21 +1,25 @@
 # Author: Kevin C Limburg
 # Description: Script for downloading the SECOM dataset from the UCI machine
 #              learning repository, https://archive.ics.uci.edu/ml/datasets/SECOM
+os<-Sys.info()["sysname"]
+if(os == "Windows") dl.method <- "auto"
+else dl.method = "curl"
+
 
 if(!file.exists("./data/secom.data")){
     download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.data",
                   "./data/secom.data",
-                  method = "curl")
+                  method = dl.method)
 }
 
 if(!file.exists("./data/secom.names")){
     download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.names",
                   "./data/secom.names",
-                  method = "curl")
+                  method = dl.method)
 }
 
 if(!file.exists("./data/secom_labels.data")){
     download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data",
                   "./data/secom_labels.data",
-                  method = "curl")
+                  method = dl.method)
 }
